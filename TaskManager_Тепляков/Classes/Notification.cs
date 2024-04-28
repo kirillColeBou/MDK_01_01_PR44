df8bd495d837ab2eec0ae467e649f598f194761f
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-04-28 20:02:05 +0500

    Разработанный классы общего-пользования. Тепляков

---
+++ TaskManager_Тепляков/Classes/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TaskManager_Тепляков.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
