b9df18ea866dd081361e526cc17ae990f14c0199
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-04-28 20:51:38 +0500

    Разработанный контекст данных для модели задач. Тепляков

---
+++ TaskManager_Тепляков/Context/TasksContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager_Тепляков.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Models.Tasks> Tasks { get; set; }
+
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql(Classes.Database.Config.connection, Classes.Database.Config.version);
+    }
+}
