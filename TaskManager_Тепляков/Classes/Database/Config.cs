32c3883289b260b8763cdcb1b918b6200a55fe83
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-04-28 19:56:26 +0500

    Созданный класс Config. Тепляков

---
+++ TaskManager_Тепляков/Classes/Database/Config.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager_Тепляков.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=ioy;uid=root;pwd=root;database=TaskManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
